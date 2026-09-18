b4f4afa7656a8468087b89d7df94466a40321538
Yenimireddy Vignesh <vignesh22032006@gmail.com>
2026-09-18 15:33:01 +0530

    update

---
+++ src/Demo/FibonacciRecursive.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class FibonacciRecursive {
+
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) 
+               + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(7));
+    }
+}
